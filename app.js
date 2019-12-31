b2ddb4ad2baf38748502a675a4047d553d5e33dd
Kien Vo <kien.vo@traveloka.com>
2019-12-31 15:50:40 +0700

    test

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ app.use('/api/v1/cards', cardRouter);
 app.use('/api/v1/transactions', transactionRouter);
 
 //serve static
-app.use(express.static('client/build'));
+app.use(express.static('build'));
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 module.exports = app;
---
+++ build/precache-manifest.5eed40efdfaa9de63390ab6cd57ed9cd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ccfdd7cde4734f727e7e554776a9c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e05da7fe88aca98a6d1",
+    "url": "/static/js/2.6e8b2a01.chunk.js"
+  },
+  {
+    "revision": "2d837a9e0a4b51bd5c7a",
+    "url": "/static/js/main.75195209.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
